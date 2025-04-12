f82962ec2a2ec958ef0b4cfe0d67786333be3a16
Danilo :D <danianto2606@gmail.com>
2025-04-12 11:36:04 -0400

    hola

---
+++ app.js
@@ -0,0 +1,17 @@
+//:D helpe me 
+
+import express from 'express'
+
+const app = express()
+
+const puerto = 3000
+
+app.get('/', (req, res) => { //
+    console.log("Recibiendo peticion")
+
+    res.send('Hola ptos :D 2')
+})
+
+app.listen(puerto, () =>{
+    console.log(`server: http://localhost:${puerto}`)
+})
