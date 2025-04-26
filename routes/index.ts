a380b155a542067821aa57b6dd2daf701683af6e
Danilo :D <danianto2606@gmail.com>
2025-04-26 16:42:35 -0400

    Tarea 1. Programacion

--- routes/index.ts
+++ routes/index.ts
@@ -4,12 +4,7 @@ const router= express.Router();
 
 router.get('/', (req, res) => { 
     res.render('index', {
-        title: 'Kasitips',
-        texto: 'Hola Mundo :D',
-        nombre: 'Danilo Antonio',
-        apellido: 'Marin Lombano',
-        cedula: '31.719.608',
-        seccion: 4
+        title: 'ReparaTech',
     })
 })
 
